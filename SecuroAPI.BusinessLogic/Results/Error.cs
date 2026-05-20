647864859fb8135ac223077c3d41b3cce8fa08aa
Popovici Andrei-Cristian <andrei-cristian.popovici@student.upt.ro>
2026-05-21 00:07:32 +0300

    Added the resuls pattern

---
+++ SecuroAPI.BusinessLogic/Results/Error.cs
@@ -0,0 +1,11 @@
+﻿namespace SecuroAPI.BusinessLogic.Results;
+/// <summary>
+/// Creates an error type for the results pattern
+/// </summary>
+/// <param name="Code"></param>
+/// <param name="Description"></param>
+public readonly record struct Error(string Code, string Description)
+{
+    public static readonly Error None = new("", "");
+    public bool IsNone => string.IsNullOrWhiteSpace(Code);
+}
