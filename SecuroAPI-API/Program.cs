27a73858cb8d3aa42d39ebf9234d8b1ed740690f
Andrei-Cristian POPOVICI <andrei-cristian.popovici@student.upt.ro>
2026-05-28 03:49:34 +0300

    Added JWT handler and infrastructure

--- SecuroAPI-API/Program.cs
+++ SecuroAPI-API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SecuroAPI_API.Constants;
 using SecuroAPI_API.Handlers;
 using SecuroAPI.BusinessLogic.Services;
@@ -25,22 +28,39 @@ builder.Services.AddDbContext<SecuroAPIDbContext>(options => options.UseSqlServe
 
 //Identity
 builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options => { })
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<SecuroAPIDbContext>();
 
 builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = AuthenticationDefaults.ApiKeyScheme;
-    options.DefaultChallengeScheme = AuthenticationDefaults.ApiKeyScheme;
-})
-    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>(AuthenticationDefaults.BasicScheme, _ => { })
-    .AddScheme<AuthenticationSchemeOptions, ApiKeyAuthenticationHandler>(AuthenticationDefaults.ApiKeyScheme, _ => { });
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration["JwtSettings:Key"])),
+            ClockSkew = TimeSpan.Zero
+        };
+    })
+
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>(AuthenticationDefaults.BasicScheme, _ => { });
+// .AddScheme<AuthenticationSchemeOptions, ApiKeyAuthenticationHandler>(AuthenticationDefaults.ApiKeyScheme, _ => { });
 
 builder.Services.AddAuthorization();
 
 // Service and Repositories
-builder.Services.AddScoped<IRepository<APIRegistry>,BaseRepository<APIRegistry>>();
-builder.Services.AddScoped<IAPIRegistryService,APIRegistryService>();
-builder.Services.AddScoped<IUserService,UserService>();
+builder.Services.AddScoped<IRepository<APIRegistry>, BaseRepository<APIRegistry>>();
+builder.Services.AddScoped<IAPIRegistryService, APIRegistryService>();
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IApiKeyValidatorService, ApiKeyValidatorService>();
 
 var app = builder.Build();
@@ -51,12 +71,9 @@ app.MapGroup("api/defaultAuth").MapIdentityApi<ApplicationUser>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();       
+    app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "0.1");
-    });
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "0.1"); });
 }
 
 app.UseHttpsRedirection();
--- SecuroAPI.BusinessLogic/Services/UserService.cs
+++ SecuroAPI.BusinessLogic/Services/UserService.cs
@@ -1,19 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SecuroAPI.BusinessLogic.Constants;
 using SecuroAPI.BusinessLogic.DTO_s.Auth;
 using SecuroAPI.BusinessLogic.Results;
 using SecuroAPI.BusinessLogic.Services.Interfaces;
 using SecuroAPI.DataAccess.Entities;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace SecuroAPI.BusinessLogic.Services;
 
 public class UserService: IUserService
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IConfiguration _configuration;
 
-    public UserService(UserManager<ApplicationUser> userManager)
+    public UserService(UserManager<ApplicationUser> userManager, IConfiguration configuration)
     {
         _userManager = userManager;
+        _configuration = configuration;
     }
     public async Task<Result<GetRegisteredUserDTO>> RegisterUserAsync(RegisterUserDTO registerUserDto)
     {
@@ -53,7 +61,44 @@ public class UserService: IUserService
         var isPasswordValid = await _userManager.CheckPasswordAsync(user, loginUserDto.Password);
         if (!isPasswordValid) return Result<string>
             .Failure(new Error( ErrorCodes.BadRequest, "Invalid Credentials" ));
+        
+        // token Issuing
+        var token = await GenerateToken(user);
 
-        return Result<string>.Success("Login Successful");
+        return Result<string>.Success(token);
+    }
+
+    private async Task<string> GenerateToken(ApplicationUser user)
+    {
+        //basic user claims
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Name, $"{user.FirstName} {user.LastName}"),
+
+        };
+        
+        //user role claims
+        var roles = await _userManager.GetRolesAsync(user);
+        var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+
+        claims = claims.Union(roleClaims).ToList();
+        //Jwt Key credentials
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        //Create and encode token
+        var token = new JwtSecurityToken(
+            issuer:_configuration["JwtSettings:Issuer"],
+            audience:_configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(_configuration["JwtSettings:DurationInMinutes"])),
+            signingCredentials: credentials
+            );
+
+        //return token value
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
