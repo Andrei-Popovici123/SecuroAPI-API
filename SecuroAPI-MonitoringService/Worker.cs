43d08dbcd7238e7f7f78501a27e7f006ead95107
Andrei-Cristian POPOVICI <andrei-cristian.popovici@student.upt.ro>
2026-08-03 02:27:38 +0300

    basic consumer and publisher

--- SecuroAPI-MonitoringService/Worker.cs
+++ SecuroAPI-MonitoringService/Worker.cs
@@ -1,9 +1,30 @@
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
 namespace SecuroAPI_MonitoringService;
 
 public class Worker(ILogger<Worker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        await using var connection = await factory.CreateConnectionAsync(stoppingToken);
+        await using var channel = await connection.CreateChannelAsync(cancellationToken: stoppingToken);
+        {
+            await channel.QueueDeclareAsync(queue: "test.jobs", durable: true,
+                exclusive: false, autoDelete: false, arguments: null, cancellationToken: stoppingToken);
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body.ToArray());
+                Console.WriteLine($"Received Message ... {message}", message);
+            };
+            
+            await channel.BasicConsumeAsync("test.jobs", true, consumer, cancellationToken: stoppingToken);
+        }
         while (!stoppingToken.IsCancellationRequested)
         {
             if (logger.IsEnabled(LogLevel.Information))
--- SecuroAPI-TestRunnerService/Worker.cs
+++ SecuroAPI-TestRunnerService/Worker.cs
@@ -1,15 +1,34 @@
+using System.Text;
+using RabbitMQ.Client;
+
 namespace SecuroAPI_TestRunnerService;
 
 public class Worker(ILogger<Worker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        await using var connection = await factory.CreateConnectionAsync(stoppingToken);
+        await using var channel = await connection.CreateChannelAsync(cancellationToken: stoppingToken);
+        {
+            await channel.QueueDeclareAsync(queue: "test.jobs", durable: true,
+                exclusive: false, autoDelete: false, arguments: null, cancellationToken: stoppingToken);
+            string message = "Hello World from Test Job!";
+            var body = Encoding.UTF8.GetBytes(message);
+            
+            await channel.BasicPublishAsync("", "test.jobs", body, cancellationToken: stoppingToken);
+            Console.WriteLine("  Sent message {0}", message);
+        }
+        
+        
+
         while (!stoppingToken.IsCancellationRequested)
         {
             if (logger.IsEnabled(LogLevel.Information))
             {
                 logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
+
             await Task.Delay(1000, stoppingToken);
         }
     }
